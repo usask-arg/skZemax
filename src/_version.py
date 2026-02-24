c1cffa409d16b681fc10b20d80d4636e91a03195
dsl935 <dsl935@mail.usask.ca>
2026-02-24 17:08:21 -0600

    testwheel

--- src/_version.py
+++ src/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.dev44+ga2f73748e.d20260224'
-__version_tuple__ = version_tuple = (0, 1, 'dev44', 'ga2f73748e.d20260224')
+__version__ = version = '0.1.dev46+g8943728fb.d20260224'
+__version_tuple__ = version_tuple = (0, 1, 'dev46', 'g8943728fb.d20260224')
 
-__commit_id__ = commit_id = 'ga2f73748e'
+__commit_id__ = commit_id = 'g8943728fb'
